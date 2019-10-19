bbdf0114df15161bf711226abae5bedc69443bcd
Willson Wang <40642117+wangwillson1@users.noreply.github.com>
2019-10-19 00:22:57 -0400

    Added information about previous price and percentage change

--- webscraper.py
+++ webscraper.py
@@ -10,7 +10,7 @@ page_html = urlClient.read()
 urlClient.close()
 
 # Function to grab prices
-def parsePrice():
+def getPrice():
     # Parsing HTML
     page_soup = soup(page_html, "html.parser")
 
@@ -18,7 +18,29 @@ def parsePrice():
     curr_price = page_soup.find('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
     return curr_price
 
+# Function to find percentage difference
+def percentageDifference(new, old):
+    return ( (new - old) / old ) * 100
+
 # Infinite loop to grab prices in real time
+first_itr = True;
+prev_price = 0;
+
 while True:
-    print('The current price is: ' + str(parsePrice()))
-    sleep(10)
+    prev_price = 0
+    curr_price = float(getPrice())
+
+    if first_itr:
+        prev_price = float(getPrice())
+        first_itr = False;
+
+    else:
+        prev_price = curr_price
+
+    # Print information to console
+    print('The current price is: ' + str(curr_price))
+    print('The previous price was: ' + str(prev_price))
+
+    percentage_change = percentageDifference(curr_price, prev_price)
+    print('The percentage change is: ' + str(percentage_change) + '%\n\n')
+    sleep(5)
